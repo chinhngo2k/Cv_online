48c6b23ddb48adec98d03f3f4144703e8ab2ea1a
minhkhang97 <maxwelljenkins2005@gmail.com>
2021-01-06 00:09:01 +0700

    cap nhat lai

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-const body = document.querySelector('.content__body');
-ScrollReveal().reveal('.timeline');
+const body = document.querySelector(".content__body");
+ScrollReveal().reveal(".timeline");
+
+const itemRight = document.querySelectorAll('.item__right ul');
+console.log(itemRight);
+
+fetch(
+  "http://localhost/CV/server/api/resume/getResumeByPersonId.php?personal_id=1"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
